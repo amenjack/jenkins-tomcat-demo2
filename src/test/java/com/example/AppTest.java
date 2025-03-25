0dccab26e960b4d4a024609f2b3a754a30e84d91
okuoim <okuoim@gmail.com>
2025-03-25 07:07:46 +0100

    Initial commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
